59f3ab37f7666e64d5e49cb778b7835c3c69f7e1
ildar-khan-94 <ildar.khan94@gmail.com>
2026-09-06 22:02:19 +0300

    first commit

--- src/main/java/academy/week01/ArrayTasks.java
+++ src/main/java/academy/week01/ArrayTasks.java
@@ -14,7 +14,7 @@ public class ArrayTasks {
     public int sum(int[] numbers) {
         int sum = 0;
         for (int n : numbers) {
-            sum *= n;
+            sum += n;
         }
         return sum;
     }
