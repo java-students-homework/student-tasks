6a345f88513ad877d56f36deb96288031e5dfcf6
academyhh <ksenia.pudelko234@gmail.com>
2026-09-08 18:22:23 +0300

    homework ifElseTaska

--- src/main/java/academy/week01/IfElseTasks.java
+++ src/main/java/academy/week01/IfElseTasks.java
@@ -58,7 +58,7 @@ public class IfElseTasks {
         if (number > 0) {
             return 1;
         } else if (number < 0)
-            return 0;
+            return -1;
         else {
             return 0;
         }
